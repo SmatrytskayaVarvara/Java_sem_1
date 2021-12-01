a6020f51457ecf22b7b5844a2db2cd387f045d7b
SmatrytskayaVarvara <74321873+SmatrytskayaVarvara@users.noreply.github.com>
2021-12-01 14:48:42 +0100

    Create Main.java

---
+++ laba8/Main.java
@@ -0,0 +1,12 @@
+package laba8;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Interface countriesCatalog = new Interface("Страны мира");
+        countriesCatalog.setLayout(null);
+        countriesCatalog.setVisible(true);
+        countriesCatalog.setResizable(false);
+    }
+}
