45fcceb9c04258e1f87a0bbd2780162f4e3ef4e8
SmatrytskayaVarvara <74321873+SmatrytskayaVarvara@users.noreply.github.com>
2021-12-01 14:45:45 +0100

    Create Main.java

---
+++ laba7/Main.java
@@ -0,0 +1,16 @@
+package laba7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Order order = new Order("Оформление заказа");
+
+        order.setVisible(true);
+
+        order.setResizable(false);
+
+        order.setLocationRelativeTo(null);
+
+    }
+
+}
