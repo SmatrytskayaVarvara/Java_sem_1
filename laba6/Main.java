60518e8b6a1dd917ccdb66ba595f02e04aa51191
SmatrytskayaVarvara <74321873+SmatrytskayaVarvara@users.noreply.github.com>
2021-12-01 14:44:09 +0100

    Create Main.java

---
+++ laba6/Main.java
@@ -0,0 +1,11 @@
+package laba6;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedEncodingException, IOException {
+        WorkWithString n = new WorkWithString();
+        n.runConsole();
+    }
+}
